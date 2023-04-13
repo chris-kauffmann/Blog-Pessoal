be1009e7dec178fc1840d3d2b279581fdcbf89c8
chris-kauffmann <chris_kauffmann@hotmail.com>
2023-04-13 14:58:48 -0300

    CRUD Parte 1

---
+++ src/main/java/com/example/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,8 @@
+package com.example.blogpessoal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.blogpessoal.model.Postagem;
+
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+
+}
